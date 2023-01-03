948d640e18eedde0d3456fd60f7789e70d7fff71
Paulo Cupertino <paulorlima06@gmail.com>
2023-01-03 19:25:13 -0300

    Testar Fachada Categorias

---
+++ src/main/java/com/paulo/petshop/resources/CategoriaResource.java
@@ -0,0 +1,17 @@
+package com.paulo.petshop.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+
+public class CategoriaResource {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String show() {
+		return "Teste REST Categorias !";
+	}
+	
+}
